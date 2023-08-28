074eb65a5665b56f2fb6c8755bc2ec167bfc149b
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-28 22:00:37 +0800

    updated randomization values

--- addons/fastnoiselite_tool/vector3_input.gd
+++ addons/fastnoiselite_tool/vector3_input.gd
@@ -7,9 +7,8 @@ signal value_changed(value: Vector3)
 @onready var y_input: SpinBox = %OffsetYInput
 @onready var z_input: SpinBox = %OffsetZInput
 
-var min_value: int = -10000
-var max_value: int = 10000
-
+@export var min_value: int = -10000
+@export var max_value: int = 10000
 
 @export var value: Vector3 = Vector3.ZERO
 @export var disabled: bool = false
